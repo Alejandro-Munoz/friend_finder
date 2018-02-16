89c0952ad3aec9100bffa23f909dbd8eb4770d7e
alejandro-munoz <alejandro.z.munoz@gmail.com>
2018-02-15 18:03:15 -0800

    htmlRoutes.js

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
-// const htmlRoutes = require("./htmlRoutes.js");
-// const apiRoutes = require("./apiRoutes");
+const htmlRoutes = require("./server/htmlRoutes.js");
+const apiRoutes = require("./server/apiRoutes");
 
 const app = express();
+
+htmlRoutes(app);
+apiRoutes(app);
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
-// htmlRoutes(app);
-// apiRoutes(app);
+htmlRoutes(app);
+apiRoutes(app);
 
 app.listen(PORT,()=>{console.log("Server started");});
 
